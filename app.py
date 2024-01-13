6d03a9f13068a4ea022843632889cbb494310a30
dhruvpanchal29 <49442460+dhruvpanchal29@users.noreply.github.com>
2024-01-13 15:23:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def input_image_setup(uploaded_file):
 
 st.set_page_config(page_title="Gemini Image Processing",layout="wide")
 st.markdown("<h1 style='text-align: center'>Gemini App ♊</h1>", unsafe_allow_html=True)
-st.markdown("*Upload an image of your choice. If you desire specific details, enter a prompt, press enter, and then click 'Describe the Image' for customized information. For a comprehensive overview, simply click the 'Describe the Image' button to receive all details about the image. Scroll down after the image to see the response*")
+st.markdown("*Upload an image of your choice. If you desire specific details, enter a prompt, press enter, and then click 'Describe the Image' for customized information. For a comprehensive overview, simply click the 'Describe the Image' button to receive all details about the image. After viewing the image, scroll down to see the response.*")
 input=st.text_input("Your Prompt: ",key="input")
 submit=st.button("Describe the Image")
 uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
